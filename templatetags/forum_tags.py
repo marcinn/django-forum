05c2678ee52ceee839d8d38818269a1a2eb474e9
ross@Rossp.org <ross@Rossp.org@8fd21cde-4a2f-0410-9917-19c3686e4e0c>
2008-12-30 02:54:34 +0000

    * Add template tag for latest posts by user.
    git-svn-id: http://django-forum.googlecode.com/svn/trunk@36 8fd21cde-4a2f-0410-9917-19c3686e4e0c

--- templatetags/forum_tags.py
+++ templatetags/forum_tags.py
@@ -52,5 +52,33 @@ class ForumLatestPostsNode(Node):
         context[self.context_var] = Post.objects.select_related().order_by('-time')[:self.number]
         return ''
 
+
+def forum_latest_user_posts(parser, token):
+    """
+    {% forum_latest_user_posts user [number] as [context_var] %}
+    """
+    bits = token.contents.split()
+    if len(bits) not in (2, 3, 5):
+        raise TemplateSyntaxError('%s tag requires one, two or four arguments' % bits[0])
+    if bits[3] != 'as':
+        raise TemplateSyntaxError("Second argument to %s tag must be 'as'" % bits[0])
+    if not bits[2]:
+        bits[2] = 5 # Default number of items
+    if not bits[3]:
+        bits[4] = 'latest_user_posts'
+    return ForumLatestUserPostsNode(bits[1], bits[2], bits[4])
+
+class ForumLatestUserPostsNode(Node):
+    def __init__(self, user, number, context_var):
+        self.user = Variable(user)
+        self.number = int(number) - 1
+        self.context_var = context_var
+    
+    def render(self, context):
+        user = self.user.resolve(context)
+        context[self.context_var] = Post.objects.select_related().filter(author=user).order_by('-time')[:self.number]
+        return ''
+
 register.tag('forum_latest_posts', forum_latest_posts)
 register.tag('forum_latest_thread_activity', forum_latest_thread_activity)
+register.tag('forum_latest_user_posts', forum_latest_user_posts)
